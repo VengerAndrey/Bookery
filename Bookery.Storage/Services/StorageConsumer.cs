83e25b4f65e1fc793b7add4d02b1213b2fe8c494
VengerAndrey <vengerandr@gmail.com>
2022-02-18 12:59:55 +0200

    Fix connection to RabbitMQ in storage microservice.

--- Bookery.Storage/Services/StorageConsumer.cs
+++ Bookery.Storage/Services/StorageConsumer.cs
@@ -6,24 +6,29 @@ namespace Bookery.Storage.Services
 {
     public class StorageConsumer : IHostedService, IDisposable
     {
-        private readonly IConnection _connection;
-        private readonly IModel _channel;
-        private readonly EventingBasicConsumer _consumer;
+        private readonly ConnectionFactory _connectionFactory;
+        private IConnection? _connection;
+        private IModel? _channel;
+        private EventingBasicConsumer? _consumer;
         private readonly IStorageService _storageService;
 
         public StorageConsumer(string hostname, int port, string username, string password, IStorageService storageService)
         {
             _storageService = storageService;
-            var factory = new ConnectionFactory()
+            _connectionFactory = new ConnectionFactory()
             {
                 HostName = hostname,
                 Port = port,
                 UserName = username,
                 Password = password
             };
-            _connection = factory.CreateConnection();
+            
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _connection = _connectionFactory.CreateConnection();
             _channel = _connection.CreateModel();
-            _channel.QueueDeclare(queue: "delete_file",
+            _channel?.QueueDeclare(queue: "delete_file",
                 durable: false, exclusive: false, autoDelete: false, arguments: null);
 
             _consumer = new EventingBasicConsumer(_channel);
@@ -32,24 +37,21 @@ namespace Bookery.Storage.Services
                 var id = Guid.Parse(Encoding.UTF8.GetString(args.Body.Span));
                 await DeleteFile(id);
             };
-        }
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            _channel.BasicConsume(queue: "delete_file", autoAck: true, consumer: _consumer);
+            _channel?.BasicConsume(queue: "delete_file", autoAck: true, consumer: _consumer);
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            _channel.Close();
-            _connection.Close();
+            _channel?.Close();
+            _connection?.Close();
             return Task.CompletedTask;
         }
 
         public void Dispose()
         {
-            _connection.Dispose();
-            _channel.Dispose();
+            _connection?.Dispose();
+            _channel?.Dispose();
         }
 
         private async Task DeleteFile(Guid id)
